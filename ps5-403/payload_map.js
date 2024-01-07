b4c3becd2ace59fb2a3c0cc8fb9cab35f1288c8d
dragon-cheatPkg-ÇáÕíÇ/goldheÇá999ps4لãäÇÝÓÉ ÇáÕíÇäÉ ÇáØÈíÉ ÇáãæÍÏÉeiÇáØÈíÉhchac/khlfÇáÕíÇäÉ ÇáØÈ/íÉgjubgFggitgTitiYÇáÕíÇufufufFäÉ hjfhdhhvg66tvjv73ÇáÕí/ÇäÉ ÇáØÈíÉ54iueSdf66fGufguttCi/bvfggFiitbihgtib gighfhdhcjgitubio/givcbnjitbnjfrv <120471333+DRAGON-CHEAT@users.noreply.github.com>
2024-01-07 17:54:27 +0300

    Add files via upload

---
+++ ps5-403/payload_map.js
@@ -0,0 +1,28 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN v3',
+            description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 4.03-4.51 Only.',
+            fileName: 'etaHEN-1.3B.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/illusion0001/libhijacker/releases/tag/1.138',
+            version: '1.3'
+        },
+        {
+            displayTitle: 'K-Stuff',
+            description: 'FW 4.03-4.51 offline. FPKG enabler',
+            fileName: 'ps5-kstuff-v1.2.bin',
+            author: 'sleirsgoevy',
+            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
+            version: '1.2'
+        },
+        {
+            displayTitle: 'PS5_Browser_appCache_remove',
+            description: 'Removes appcache from browser.',
+            fileName: 'Browser_appCache_remove.elf',
+            author: 'Storm21CH',
+            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
+            version: '1.0fix'
+        }
+
+    ];
